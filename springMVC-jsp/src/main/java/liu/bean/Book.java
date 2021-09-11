fa059e360258f84caf45ac0eef1840205b3e84d3
veritas <1820819809@qq.com>
2021-09-11 23:18:32 +0800

    version 1

---
+++ springMVC-jsp/src/main/java/liu/bean/Book.java
@@ -0,0 +1,53 @@
+package liu.bean;
+
+public class Book {
+    int id;
+    String title;
+    String author;
+    double price;
+    int sales;
+
+    public Book() {
+        System.out.println("创建一个book对象");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+}
